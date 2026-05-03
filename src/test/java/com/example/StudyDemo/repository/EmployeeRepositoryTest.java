cfbbb1a6d676ebc6bfe542f79c85f2d9db80b8d9
npsam220 <npsam220@gmail.com>
2026-05-03 16:01:30 +0900

    fix CI test profile

--- src/test/java/com/example/StudyDemo/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/example/StudyDemo/repository/EmployeeRepositoryTest.java
@@ -9,9 +9,11 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @DataJpaTest
+@ActiveProfiles("test")
 class EmployeeRepositoryTest {
 
     @Autowired
